7293a7addb883acf3ce1990ffc8b8252ba5837fe
Divya <divyasrivastava1113@gmail.com>
2022-08-06 05:46:03 +0530

    _07_prototype start/stop addEventListener

--- 01Basics/Day4.js
+++ 01Basics/Day4.js
@@ -1,6 +1,4 @@
-//completed splice1
 
-//start and stop constructor variable
 function cf(){
     //library which generates a random value    
 
@@ -29,21 +27,27 @@ function cf(){
     //to get Value print that value which is last registered
     this.getValue = function () {
         console.log(this.value);
-    }
+    };
+ 
+    //There are n callbacks. Array of callback functions
+    let callbacks = []; 
+    // use the latest value in those callback functions
 
+    //When user calls a function through eventListener
+    this.addEventListener = function (callbackFunction) {
+    //The browser should loop through the callbacks array
+        callbacks.push(callbackFunction);
+        setTimeout(()=>{
+            callbacks.forEach(()=>{callbackFunction(this.value)});
+        },1000);       
+
+    };
+    
+    
 }
 const obj= new cf();
-obj.addEventListener
+obj.addEventListener((val1)=>{console.log('In 1st Listener' + val1)});
+obj.addEventListener((val2)=>{console.log('In 2nd Listener' + val2)});
+obj.addEventListener((val3)=>{console.log('In 3rd Listener'+ val3)});
 
 
-
-// 4 states : start start() Do nothing
-// stop() start() Start
-// start stop() clearInterval
-// stopped stop() Dp nothing
-
-
-// On creation of the Object, 
-// 1. random value is assigned
-// 2. random value changes every one second 
-// 3. on stopping, the timer as well as the value assignment should stop 
